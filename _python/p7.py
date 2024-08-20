1b9fac9ec0bf077be7a6e2a7c0685fe944b3fa5a
sushmakokati <kokatisushma@gmail.com>
2024-08-20 22:08:04 +0530

    Update _python

---
+++ _python/p7.py
@@ -0,0 +1,6 @@
+alphabet=input('Enter the alphabet:')
+
+if alphabet==a or alphabet==e or alphabet==i or alphabet==o or alphabet==u:
+    print('The alphabet is vowel')
+else:
+    print('The alphabet is consonant')
