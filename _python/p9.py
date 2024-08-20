1f0a5ec6f2c9c22df404a9aac800ada042282ba4
sushmakokati <kokatisushma@gmail.com>
2024-08-20 22:34:30 +0530

    Update p9.py

--- _python/p9.py
+++ _python/p9.py
@@ -1,4 +1,4 @@
-#Program to create a multiplication table of a number 
+ #Program to create a multiplication table of a number 
 
 number= int(input('Enter the number to create multiplication table '))
 print('multiplication table of', number)
