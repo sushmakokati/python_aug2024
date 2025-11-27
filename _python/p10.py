5175b668d0b4b4ebb734e98daf1b440c28b4c89c
sushmakokati <kokatisushma@gmail.com>
2025-11-27 11:01:17 +0530

    Delete _python/p10.py

--- _python/p10.py
+++
@@ -1,6 +0,0 @@
-#Program to check if the alphabet is Vowel or Consonant
-alphabet = input('Enter the alphabet:')
-if alphabet ==a or alphabet ==e or alphabet ==i or alphabet ==o or alphabet ==u:
-    print("The alphabet is vowel")
-else:
-    print("The alphabet is consonant")
